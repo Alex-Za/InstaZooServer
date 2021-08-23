baece93220c04be8d888c6cbcb455b4b56012cfe
Alex-Za <argiluckone@gmail.com>
2021-08-23 11:19:24 +0300

    check

--- server/src/main/java/com/example/demo/SpringAngularAppApplication.java
+++ server/src/main/java/com/example/demo/SpringAngularAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringAngularAppApplication {
-	//test
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAngularAppApplication.class, args);
